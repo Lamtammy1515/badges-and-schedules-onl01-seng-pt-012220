023d8f90386a56bfebf1a9ab02e945841062156e
Tammy Lam <lamtammy1515@gmail.com>
2020-01-25 19:38:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,9 +16,9 @@ end
 
 
 def assign_rooms(attendees)
-   room_assignments = []
-   attendees.each.with_index do |name, index|
-   room_assignments <<  "Hello, #{name}! You'll be assigned to room #{index+1}!"
+   
+   attendees.map.with_index do |name, index|
+   "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end 
   return room_assignments
 end 
